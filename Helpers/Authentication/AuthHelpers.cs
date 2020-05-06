b37be60eb65a7c04946242b7a59b6e940714d749
lin.lo@husky.neu.edu <lin.lo@husky.neu.edu>
2020-05-05 22:14:23 -0400

    helper project was in the wrong location

---
+++ Helpers/Authentication/AuthHelpers.cs
@@ -0,0 +1,74 @@
+﻿using Entities.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Helpers.Auth
+{
+    public class AuthHelpers
+    {
+        public static JwtSecurityToken GenerateToken(User user, IConfiguration config)
+        {
+            var authClaims = new[]
+               {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Name, user.UserName)
+                };
+
+            var secret = config["Auth:Secret"];
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret));
+
+            var issuer = config["Auth:Issuer"];
+            var audience = config["Auth:Audience"];
+            var durationInMinutes = Convert.ToDouble(config["Auth:Duration"]);
+
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                expires: DateTime.Now.AddMinutes(durationInMinutes),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
+
+            return token;
+        }
+
+        public static ClaimsPrincipal GetPrincipalFromExpiredToken(string token, IConfiguration config)
+        {
+            var secret = config["Auth:Secret"];
+            var issuer = config["Auth:Issuer"];
+            var audience = config["Auth:Audience"];
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+                ValidIssuer = issuer,
+                ValidAudience = audience,
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if (!(securityToken is JwtSecurityToken jwtSecurityToken) || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Invalid token");
+
+            return principal;
+        }
+
+        public static string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[32];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+    }
+}
